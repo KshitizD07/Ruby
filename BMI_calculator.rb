b3961f2047d906a72d292c1af60f3aa9d4cce48c
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-06-22 17:21:21 +0530

    Enhance BMI calculation flow with user prompts and improved output formatting

--- BMI_calculator.rb
+++ BMI_calculator.rb
@@ -10,9 +10,68 @@ $pastel = Pastel.new
 
 #Placeholder methods to fill in later. These methods will be used to calculate BMI and display the reuslts in a table format.
 
-def calculate_bmi_flow
-    puts $pastel.yellow("Calculating BMI...")
+#Claculates BMI
+def calculate_bmi(weight, height)
+    (weight / (height**2)).round(2)
 end
+
+    #Return status based on BMI value
+def get_calssification_details(bmi)
+    if bmi < 18.5
+        { status: "Underweight", color: :yellow }
+    elsif bmi >= 18.5 && bmi <24.9
+        { status: "Normal weight", color: :green }
+    elsif bmi >= 25 && bmi < 29.9
+        { status: "Overweight", color: :magenta }
+    else
+            { status: "Obese", color: :red }
+    end
+end    
+
+def calculate_bmi_flow
+    puts "\n"
+    puts $pastel.cyan.bold("----BMI Calculator ---")
+
+    #Asking for name
+    name = $prompt.ask("Enter your name:") do |q|
+        q.required true
+        q.modify :capitalize
+    end
+
+    #Asking for weight with validation and automatice conversion to float.
+    weight = $prompt.ask("Enter your weight in kg (e.g. 72.5):") do |q|
+        q.required true
+        q.validate(/^\d+(\.\d+)?$/, "Please enter a valid positive number.")
+        q.convert :float
+    end
+
+    #Asking height
+    height = $prompt.ask("Enter your height in meters (e.g. 1.75):") do |q|
+        q.required true
+        q.validate(/^\d+(\.\d+)?$/, "Please enter a valid positive number.")
+        q.convert :float
+    end
+
+    #Perform calculations
+    bmi = calculate_bmi(weight, height)
+    details = get_calssification_details(bmi)
+
+    #Displaying 
+    puts "\n"
+    puts $pastel.cyan.bold("="*35)
+    puts "  RESULTS FOR #{name.upcase}:"
+    puts $pastel.cyan.bold("=" *35)
+    puts "  Weight:          #{weight} kg"
+    puts "  Height:          #{height} m"
+    puts "  Calculated BMI:  #{$pastel.bold(bmi)}"
+    
+    
+    status_colored = $pastel.send(details[:color]).bold(details[:status])
+    puts "  Classification:  #{status_colored}"
+    puts $pastel.cyan.bold("="*35)
+    puts "\n"
+end
+
 
 def show_classification_table
     puts $pastel.yellow("Displaying BMI Classification Table...")
