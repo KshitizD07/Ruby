5b2f1cb65fa5a3e6774ccc96022922806bda00be
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-06-22 16:46:41 +0530

    Enhance BMI classification table with headings, title, and improved styling

--- BMI_calculator.rb
+++ BMI_calculator.rb
@@ -16,15 +16,43 @@ end
 
 def show_classification_table
     puts $pastel.yellow("Displaying BMI Classification Table...")
+    puts "\n"
+
+    #Defining headings and rows
+    headings = ['BMI Range', "Classification", "Health Risk"]
+    rows=[
+        ["Under 18.5", "Underweight", "Minimal to increased"],
+        ["18.5 - 24.9", "Normal Weight", "Minimal"],
+        ["25.0 - 29.9", "Overweight", "Increased"],
+        ["30.0 and above", "Obese", "High to Extremely High"]
+    ]
+
+    #Building the table object
+    table = Terminal::Table.new do |t|
+        t.title = $pastel.cyan.bold("BMI Classification Reference")
+        t.headings = headings.map { |h| $pastel.bold.italic(h) }
+        t.rows = rows
+
+        #Addidng styling options
+        t.style = {
+            border: :unicode,
+            alignment: :center,
+            all_separators: true
+        }
+    end
+
+    #Prints the table
+    puts table
+    puts "\n"
 end
 
 
 #This welcomes the user with a nice message when they start the program.
 def show_welcome_screen
     system("clear") || system("cls")  #This line clears the terminal screen before displaying the welcome message. It works for both Unix-based and windows ssytems.
-    puts $pastel.cyan.bold("="*45)
+    puts $pastel.cyan.bold("="*50)
     puts $pastel.cyan.bold("     WELCOME TO THE PREMIUM BMI CALCULATOR!!     ")
-    puts $pastel.cyan.bold("="*45)
+    puts $pastel.cyan.bold("="*50)
     puts "\n"
 end
 
