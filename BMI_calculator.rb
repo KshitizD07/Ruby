c0eb8c0619171d85855160d905c111086e7601c9
KSHITIZ DIXIT <kshitizd171@gmail.com>
2026-06-23 21:59:57 +0530

    Add BMI history tracking and display functionality

--- BMI_calculator.rb
+++ BMI_calculator.rb
@@ -2,6 +2,7 @@ require 'tty-prompt'
 require 'pastel'
 require 'terminal-table'
 require 'bundler/setup'
+require 'json'
 
 #Initialize helper classes from the gems we imported.
 $prompt = TTY::Prompt.new
@@ -56,6 +57,9 @@ def calculate_bmi_flow
     bmi = calculate_bmi(weight, height)
     details = get_calssification_details(bmi)
 
+    #Saving the result
+    save_result_to_history(name, weight, height, bmi, details[:status])
+
     #Displaying 
     puts "\n"
     puts $pastel.cyan.bold("="*35)
@@ -72,6 +76,29 @@ def calculate_bmi_flow
     puts "\n"
 end
 
+def save_result_to_history(name, weight, height, bmi, status)
+    new_record = {
+        name: name,
+        weight: weight,
+        height: height,
+        bmi: bmi,
+        status: status,
+        date: Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    }
+
+    #Load existing history
+    history = []
+    if File.exist?("bmi_history.json")
+        file_content = File.read("bmi_history.json")
+        history = JSON.parse(file_content)
+    end
+
+    #Adding the new record to our history array
+    history << new_record
+
+    #Save updated history back to the file
+    File.write("bmi_history.json", JSON.pretty_generate(history))
+end
 
 def show_classification_table
     puts $pastel.yellow("Displaying BMI Classification Table...")
@@ -106,6 +133,53 @@ def show_classification_table
 end
 
 
+def show_history
+    system("clear") || system("cls")
+    puts $pastel.cyan.bold("=" *50)
+    puts $pastel.cyan.bold("          YOUR BMI HISTORY          ")
+    puts $pastel.cyan.bold("=" *50)
+    puts "\n"
+
+    #Checking if history exists
+    if !File.exist?("bmi_history.json")
+        puts $pastel.yellow("Bo history found yet. GO calculate your BMI first!")
+        puts "\n"
+        return 
+    end
+
+    #Reading and parsing the data
+    file_content = File.read("bmi_history.json")
+    history = JSON.parse(file_content)
+
+    #Handling the edge case where the file exists but is somehow empty
+    if history.empty?
+        puts $pastel.yellow("No history found yet. Go calculate your BMI first!")
+        puts "\n"
+        return 
+    end
+
+    #Preparing the rows for our table
+    rows = []
+    history.each do |record|
+        rows << [record["date"], record["name"], record["weight"], record["height"], record["bmi"], record["status"]]
+    end
+
+    #Building and printing the table
+    table = Terminal::Table.new do |t|
+        t.headings = ["Date", "Name", "Weight(kg)", "Height(m)", "BMI", "Status"].map { |h| $pastel.bold.italic(h) }
+        t.rows = rows
+        t.style = {
+            border: :unicode,
+            alignment: :center,
+            all_separators: true
+        }
+    end
+
+    puts table
+    puts "\n"
+end
+    
+
 #This welcomes the user with a nice message when they start the program.
 def show_welcome_screen
     system("clear") || system("cls")  #This line clears the terminal screen before displaying the welcome message. It works for both Unix-based and windows ssytems.
@@ -123,6 +197,7 @@ def start_app
         #Renders an interactive arrow-key menu
         choice = $prompt.select("What would you like to do?") do |menu|
             menu.choice "Calculate BMI", :calculate
+            menu.choice "View History", :history
             menu.choice "View Classification Table", :table
             menu.choice "Exit", :exit
         end
@@ -130,6 +205,9 @@ def start_app
         case choice
         when :calculate
             calculate_bmi_flow
+            $prompt.keypress("Press any key to return to main menu...")
+        when :history
+            show_history
             $prompt.keypress("Press any key to return to main menu...")
         when :table
             show_classification_table
